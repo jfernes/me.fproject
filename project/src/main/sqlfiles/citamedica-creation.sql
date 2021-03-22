23f88c58411942714bb68b1c129c5f24ac42e690
jfernes <julian.fernandeze@um.es>
2021-03-22 18:58:06 +0100

    sql actualizado

--- project/src/main/sqlfiles/citamedica-creation.sql
+++ project/src/main/sqlfiles/citamedica-creation.sql
@@ -8,47 +8,47 @@ CREATE TABLE USUARIO(
 );
 
 CREATE TABLE PACIENTE(
-    usuario 	NUMBER(20,0) 	NOT NULL,
+    id 	NUMBER(20,0) 	NOT NULL,
 	nss 		VARCHAR2(11)	NOT NULL,
-	numTarjeta	VARCHAR2(20) 	NOT NULL,
+	num_tarjeta	VARCHAR2(20) 	NOT NULL,
 	telefono 	VARCHAR2(9) 	NOT NULL,
 	direccion	VARCHAR2(100)	NOT NULL,
-	CONSTRAINT paciente_pk PRIMARY KEY(usuario),
-    CONSTRAINT pac_fk_usu FOREIGN KEY (usuario) REFERENCES USUARIO(id)
+	CONSTRAINT paciente_pk PRIMARY KEY(id),
+    CONSTRAINT pac_fk_usu FOREIGN KEY (id) REFERENCES USUARIO(id)
 );
 
 CREATE TABLE MEDICO(
-    usuario 	NUMBER(20,0) 	NOT NULL,
-	numColegiado	VARCHAR2(9)		NOT NULL,
-	CONSTRAINT medico_pk PRIMARY KEY(usuario),
-    CONSTRAINT med_fk_usu FOREIGN KEY (usuario) REFERENCES USUARIO(id)
+    id 	NUMBER(20,0) 	NOT NULL,
+	num_colegiado	VARCHAR2(9)		NOT NULL,
+	CONSTRAINT medico_pk PRIMARY KEY(id),
+    CONSTRAINT med_fk_usu FOREIGN KEY (id) REFERENCES USUARIO(id)
 );
 
 CREATE TABLE MEDICO_PACIENTE(
 	medico 		NUMBER(20,0) NOT NULL,
 	paciente 	NUMBER(20,0) NOT NULL,
 	CONSTRAINT medpac_pk PRIMARY KEY(medico),
-	CONSTRAINT medpac_fk_med FOREIGN KEY(medico) REFERENCES MEDICO(usuario),
-	CONSTRAINT medpac_fk_pac FOREIGN KEY(paciente) REFERENCES PACIENTE(usuario)
+	CONSTRAINT medpac_fk_med FOREIGN KEY(medico) REFERENCES MEDICO(id),
+	CONSTRAINT medpac_fk_pac FOREIGN KEY(paciente) REFERENCES PACIENTE(id)
 );
 
 CREATE TABLE CITA(
 	id NUMBER(20,0) 	NOT NULL, --todas los valores posibles de un long
-	motivoCita VARCHAR2(500) NOT NULL,
-	fechaHora DATE NOT NULL, 
+	motivo_cita VARCHAR2(500) NOT NULL,
+	fecha_hora DATE NOT NULL, 
 	diagnostico NUMBER(20,0) NULL,
     medico NUMBER(20,0) NOT NULL,
     paciente NUMBER(20,0) 	NOT NULL,
 	CONSTRAINT cita_pk PRIMARY KEY(id),
-    CONSTRAINT cita_fk_med FOREIGN KEY (medico) REFERENCES MEDICO(usuario),
-    CONSTRAINT cita_fk_pac FOREIGN KEY (paciente) REFERENCES PACIENTE(usuario)
+    CONSTRAINT cita_fk_med FOREIGN KEY (medico) REFERENCES MEDICO(id),
+    CONSTRAINT cita_fk_pac FOREIGN KEY (paciente) REFERENCES PACIENTE(id)
 	--FOREIGN KEY(diagnostico) REFERENCES DIAGNOSTICO(id) se añade despues modificando la tabla
     --porque se hace referencia a una tabla que no existe actualmente
 );
 
 CREATE TABLE DIAGNOSTICO(
 	id NUMBER(20) NOT NULL,
-	valoracionEspecialista VARCHAR2(500) NOT NULL,
+	valoracion_especialista VARCHAR2(500) NOT NULL,
 	enfermedad VARCHAR2(100) NOT NULL,
 	cita NUMBER(20) NOT NULL,
 	CONSTRAINT diag_pk PRIMARY KEY(id),
